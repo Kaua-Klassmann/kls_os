22cabf4162cbe59b0ac6ceb412213718e512d050
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-09-02 22:45:41 -0300

    macros test

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@
 #![no_main]
 
 mod panic;
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
-
-    for (index, &byte) in b"klsOS".into_iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(index as isize * 2) = byte;
-            *vga_buffer.offset(index as isize * 2 + 1) = 0x2;
-        }
+    for i in 1..=30 {
+        println!("Teste{}", i);
     }
 
     loop {}
