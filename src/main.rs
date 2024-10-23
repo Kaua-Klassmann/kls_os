df0ab1c9b03c285966348dce86c07940d7c72a1c
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-10-22 21:17:49 -0300

    Added cpu exceptions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #![no_std]
 #![no_main]
 
-mod panic;
-mod vga_buffer;
+use kls_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     for i in 1..=30 {
         println!("klsOS - {}", i);
     }
+
+    kls_os::init();
+
+    println!("Nao crashou!");
 
     loop {}
 }
