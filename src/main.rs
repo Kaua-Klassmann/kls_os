36fc61f79955d309844ed3c13d394cc227d9994c
Kaua Klassmann <131408936+Kaua-Klassmann@users.noreply.github.com>
2024-10-23 04:38:39 -0600

    Updated of order boot

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,11 @@ use kls_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    kls_os::init();
+
     for i in 1..=30 {
         println!("klsOS - {}", i);
     }
-
-    kls_os::init();
-
-    println!("Nao crashou!");
 
     loop {}
 }
